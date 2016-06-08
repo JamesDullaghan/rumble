7c2f56e2e9edae741854aeb697632ad3a3c72fac
James <james@fldlvl.com>
2016-06-07 20:14:30 -0700

    Modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,31 +5,37 @@ var env = process.env.MIX_ENV || 'dev'
 var prod = env === 'prod'
 
 var basePath = './web/static/js/'
-var entry = basePath + 'bundle.js'
-var entry2 = basePath + 'phoenix.js'
+var bundlePath = basePath + 'bundle.js'
+var phoenixPath = basePath + 'phoenix.js'
+var outputPath = path.join(__dirname, './priv/static/js')
+
 var plugins = [new webpack.NoErrorsPlugin()]
 var loaders = ["babel?presets[]=es2015,presets[]=stage-0,presets[]=react"]
 var publicPath = 'http://localhost:4001/'
 
+var devtool, entry;
+
 if (prod) {
   plugins.push(new webpack.optimize.UglifyJsPlugin())
+  devtool = null
+  entry = bundlePath
 } else {
   plugins.push(new webpack.HotModuleReplacementPlugin())
   loaders.unshift('react-hot')
+  devtool = 'eval-sourcemaps'
+  entry = [
+    'webpack-dev-server/client?' + publicPath,
+    'webpack/hot/only-dev-server',
+    bundlePath,
+    phoenixPath
+  ]
 }
 
 module.exports = {
-  devtool: prod ? null : 'eval-sourcemaps',
-  entry: prod ? entry : {
-    bundle: [
-      'webpack-dev-server/client?' + publicPath,
-      'webpack/hot/only-dev-server',
-      entry
-    ],
-    phoenix: entry2
-  },
+  devtool: devtool,
+  entry: entry,
   output: {
-    path: path.join(__dirname, './priv/static/js'),
+    path: outputPath,
     filename: 'bundle.js',
     publicPath: publicPath
   },
